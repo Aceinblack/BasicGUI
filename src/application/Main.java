45ec5762d447b18df1f8c10feb357a73da900504
Brinsly <ybrinsly@gmail.com>
2024-08-28 11:44:52 -0500

    Finished program

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,52 +1,91 @@
 package application;
-	
+
 
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CustomMenuItem;
+import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
 
-import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.time.LocalDate;
+
 import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.ZonedDateTime;
+import java.util.Random;
 
-import javax.swing.Action;
-
-import java.time.ZoneId;
-
-
+/**
+ * Creates a simply GUI using JavaFX
+ * @author Brinsly Yendeh
+ * @version 2.0 08/28/2024
+ */
+		
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			
+			Label displayLabel = new Label("Date & Time:");
 			TextField display = new TextField();
 			display.setEditable(false);
 			
+			//The buttons 2 be added to the drop down menu
 			Button option1 = new Button("Display date and time");
 			Button option2 = new Button("Print to file");
-			Button option3 = new Button("Change Font");
+			Button option3 = new Button("Change Background Font");
 			Button option4 = new Button("Exit");
 			
-			Menu menu = new Menu();
+			option1.setPrefWidth(200);
+			option2.setPrefWidth(200);
+			option3.setPrefWidth(200);
+			option4.setPrefWidth(200);
+			
+			//create menu object and menu sub options
+			//and assign the buttons to them
+			Menu menu = new Menu("Menu");
 			CustomMenuItem menu1 = new CustomMenuItem(option1);
 			CustomMenuItem menu2 = new CustomMenuItem(option2);
 			CustomMenuItem menu3 = new CustomMenuItem(option3);
-			CustomMenuItem menu3 = new CustomMenuItem(option3);
+			CustomMenuItem menu4 = new CustomMenuItem(option4);
 			
+			//pass all the menuItems to the menu object
+			//create the menu bar and pass the menu to it
+			menu.getItems().addAll(menu1, menu2, menu3, menu4);
+			MenuBar bar = new MenuBar();
+			bar.getMenus().add(menu);
+			bar.setPrefWidth(330);
+			
+			//Create a layout and add the created components
+			//to it
+			GridPane root = new GridPane();
+			root.setPadding(new Insets(10, 10, 10, 10));
+			root.setHgap(10);
+		    root.setVgap(10);
+			root.add(bar, 0, 0);
+			root.add(displayLabel, 0, 1);
+			root.add(display, 0, 2);
+			
+			//create a scene and pass it the layout
+			//as well as its size then pass that scene to
+			//the stage and show the stage
+			Scene scene = new Scene(root,350,350);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			
+			//displays date and time to TextField when menu1 is clicked
 			option1.setOnAction((EventHandler<ActionEvent>) new EventHandler<ActionEvent>() {
 				@Override
 		         public void handle(ActionEvent event) {
@@ -54,6 +93,7 @@ public class Main extends Application {
 				}
 			});
 			
+			//Writes the TextField's content to a file when menu2 is clicked
 			option2.setOnAction((EventHandler<ActionEvent>) new EventHandler<ActionEvent>() {
 				@Override
 		         public void handle(ActionEvent event) {
@@ -64,6 +104,8 @@ public class Main extends Application {
 						PrintWriter(file);
 						
 						write.println(display.getText());
+						display.setText(""); 
+						displayLabel.setText("Successfully printed date and time to file log.txt"); 
 						write.close();
 					} catch (IOException e) {
 						// TODO Auto-generated catch block
@@ -73,15 +115,28 @@ public class Main extends Application {
 				}
 			});
 			
+			//create a random number and with it create a random hue of the color green
+			//by string rgb to (0 red, random green, 0 blue). The + 56 is to account
+			//for hue's too dark and make them visibly more green
 			option3.setOnAction((EventHandler<ActionEvent>) new EventHandler<ActionEvent>() {
 				@Override
 		         public void handle(ActionEvent event) {
-					display.setText("" + LocalDateTime.now()); 
+					Random random = new Random();
+			        int hue = random.nextInt(206) + 50;
+			        Color color = Color.rgb(0, hue, 0);
+			        root.setBackground(new Background(new BackgroundFill(color, null, null)));
+			        display.setText("Background color changed. Hue: " + hue); 
 				}
 			});
 			
-			primaryStage.setScene(scene);
-			primaryStage.show();
+			//ends the program
+			option4.setOnAction((EventHandler<ActionEvent>) new EventHandler<ActionEvent>() {
+				@Override
+		         public void handle(ActionEvent event) {
+					Platform.exit(); 
+				}
+			});
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
